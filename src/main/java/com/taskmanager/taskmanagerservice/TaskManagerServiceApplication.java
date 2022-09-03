d8c5acfc328424eaf4b25def2cddae58da05d3bf
Ak-Shaw <ayushshawz@gmail.com>
2022-09-03 09:27:55 +0530

    chore(repo): initialize project using spring initializr

---
+++ src/main/java/com/taskmanager/taskmanagerservice/TaskManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.taskmanager.taskmanagerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taskmanager/taskmanagerservice/TaskManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmanager.taskmanagerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
